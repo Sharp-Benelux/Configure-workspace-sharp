1dee9ef3e4e00dfd26c7926915e45678410ea8b0
Sharp-Benelux <tim.bosch@sharp.eu>
2023-01-12 10:26:06 +0100

    Update Herstartmelding.ps1

--- Herstartmelding.ps1
+++ Herstartmelding.ps1
@@ -16,7 +16,7 @@ if (!$ProtocolHandler) {
 
 #onderstaande stuurt de scripting weg naar de ingelogde gebruikers met de juiste foto's, voor meer informatie over de documentatie en parameters van dit script zie https://github.com/Windos/BurntToast
 $scriptblock = {
-    $heroimage = New-BTImage -Source 'https://coopervisionspecialtylenses.com/wp-content/uploads/2021/01/cvi-sec-procornea-green-1.png' -HeroImage
+    $heroimage = New-BTImage -Source 'https://www.deopticien.biz/images/featured_news/pro.jpg' -HeroImage
     $Text1 = New-BTText -Content  "Uw computer wordt na 10 minuten opnieuw opgestart"
     $Text2 = New-BTText -Content "Sharp IT services heeft updates geïnstalleerd. U kunt hieronder kiezen voor nu herstarten of het uitstellen hiervan"
     $Button = New-BTButton -Content "Uitstellen" -snooze -id 'SnoozeTime'
